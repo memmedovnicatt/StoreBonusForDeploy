41b57abf578d544a584f7913f946e362b2899d63
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-26 17:07:36 +0400

    checkCompanyExists method was added

--- src/main/java/com/nicat/storebonus/services/CompanyService.java
+++ src/main/java/com/nicat/storebonus/services/CompanyService.java
@@ -1,10 +1,14 @@
 package com.nicat.storebonus.services;
 
 import com.nicat.storebonus.dtos.request.CompanyRequest;
+import com.nicat.storebonus.entities.Company;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CompanyService {
     void create(@Valid CompanyRequest companyRequest);
+
+    Company checkCompanyExists(Long companyId);
 }
