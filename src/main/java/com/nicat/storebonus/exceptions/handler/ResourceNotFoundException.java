7dc7aba72bd9a1f8aed39c01bd9fe97e95929424
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-27 11:47:57 +0400

    fixed ResourceNotFoundException

--- src/main/java/com/nicat/storebonus/exceptions/handler/ResourceNotFoundException.java
+++ src/main/java/com/nicat/storebonus/exceptions/handler/ResourceNotFoundException.java
@@ -1,10 +1,6 @@
 package com.nicat.storebonus.exceptions.handler;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
     public ResourceNotFoundException(String resourceName, Object field) {
         super(String.format("%s not found with: %s", resourceName, field));
     }
