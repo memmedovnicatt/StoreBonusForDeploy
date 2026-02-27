84c786cddfa3807259e2c0a487b68fc1d68a276f
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-27 11:46:56 +0400

    fixed GlobalHandlerException and was added handleAlreadyExists method

--- src/main/java/com/nicat/storebonus/exceptions/GlobalHandlerException.java
+++ src/main/java/com/nicat/storebonus/exceptions/GlobalHandlerException.java
@@ -2,6 +2,7 @@ package com.nicat.storebonus.exceptions;
 
 import com.nicat.storebonus.dtos.response.ApiResponse;
 import com.nicat.storebonus.dtos.response.ResponseMessage;
+import com.nicat.storebonus.exceptions.handler.ResourceAlreadyExistsException;
 import com.nicat.storebonus.exceptions.handler.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,42 +16,6 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
-
-    //for manage all not found exceptions
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<Void>> handleNotFound(ResourceNotFoundException ex) {
-        ApiResponse<Void> response = ApiResponse.<Void>builder()
-                .success(false)
-                .message(ex.getMessage())
-                .code(HttpStatus.NOT_FOUND.value())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(response);
-    }
-
-
-    //for manage validation errors
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage()));
-
-        ApiResponse<Map<String, String>> response = ApiResponse.<Map<String, String>>builder()
-                .success(false)
-                .message("Validation failed")
-                .code(4000)
-                .data(errors)
-                .timestamp(LocalDateTime.now())
-                .build();
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(response);
-    }
 
     //for manage system errors
     @ExceptionHandler(Exception.class)
@@ -66,5 +31,54 @@ public class GlobalHandlerException {
                         .data(null)
                         .timestamp(LocalDateTime.now())
                         .build());
+    }
+
+    //for manage all not found exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNotFound() {
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .success(false)
+                .message(ResponseMessage.NOT_FOUND.getMessage())
+                .code(ResponseMessage.NOT_FOUND.getCode())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+
+    //for manage validation errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        ApiResponse<Map<String, String>> response = ApiResponse.<Map<String, String>>builder()
+                .success(false)
+                .message(ResponseMessage.VALIDATION_ERROR.getMessage())
+                .code(ResponseMessage.VALIDATION_ERROR.getCode())
+                .data(errors)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+
+    //for manage all already exists exceptions
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<ApiResponse<Void>> handleAlreadyExists() {
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .success(false)
+                .message(ResponseMessage.ALREADY_EXISTS.getMessage())
+                .code(ResponseMessage.ALREADY_EXISTS.getCode())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 }
